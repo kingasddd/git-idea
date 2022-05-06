a0067cd2b18afca5fb25122e13982763dd68bd95
root <3238018431@qq.com>
2022-05-06 11:44:51 +0800

    first commit

---
+++ src/main/java/com/wpsedu/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.wpsedu.git;
+
+/**
+ * @author 吴鹏帅
+ * @version 1.0
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
